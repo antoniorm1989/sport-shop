628de133b404db011b9896849b5ec2a80b0697bd
Antonio Alonso Rosales Martinez <antonio-cam2@hotmail.com>
2019-10-27 07:25:12 -0700

    title store

--- sport-shop-angular/src/app/app.module.ts
+++ sport-shop-angular/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: 'product-list', component: ProductListComponent },
     ])
   ],
   declarations: [
